327e8e2f1082145c1e80c88f65634a5b49428d96
Harsha patil <harshappatil2004@gmail.com>
2025-08-24 14:45:46 +0530

    added logo name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -100,7 +100,7 @@ const NavBar = () => {
                 > */}
                 
                 <h1 className="text-xl md:text-2xl font-bold ">
-                  Your Logo
+                  NextLev
                 </h1>
               </div>
             </Link>
