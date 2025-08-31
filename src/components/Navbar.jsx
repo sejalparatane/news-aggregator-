b03aca91bcd960f36c7f2355c61fa8639d1387a1
Sejal Paratane <sejal04paratane@gmail.com>
2025-08-31 01:19:10 -0700

    removed blog section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,6 +63,7 @@ const NavBar = () => {
     { name: "About", href: "/about" },
     { name: "Services", href: "/services" },
     // { name: "Blog", href: "/blog" },
+    { name: "chatbot", href: "/" },
     { name: "Contact", href: "/contact" },
   ];
 
